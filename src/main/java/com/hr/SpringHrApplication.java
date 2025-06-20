25d8c39fd63bb807b52fd1754a95067bf30d45a3
Esmail Sameh <esmil.sameh2005@gmail.com>
2025-06-20 18:26:04 +0300

    first commit

---
+++ src/main/java/com/hr/SpringHrApplication.java
@@ -0,0 +1,13 @@
+package com.hr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHrApplication.class, args);
+	}
+
+}
